0d42b2683247d6c6dc041465e9ff828de6a69b7b
Mosunov Anton <antonmossunov@gmail.com>
2022-11-17 22:36:34 +0600

    Создание приложения webapp.
    Реализована маршрутизация
    Создан шаблон с инпутами для чисел
    Подключены ранее реализованные представления
    Создана директория staticb и в ней создан файл calculate.js для реализации функционала
    В шаблоне index.html создано подключение к этому файлу с скриптом

--- calculator/calculator/urls.py
+++ calculator/calculator/urls.py
@@ -1,7 +1,8 @@
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
-from core.views import add, subtract, multiply, divide
-
+from django.urls import path, include
+from calculator.views import add, subtract, multiply, divide
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +10,5 @@ urlpatterns = [
     path('subtract/', subtract, name='subtract'),
     path('multiply/', multiply, name='multiply'),
     path('divide/', divide, name='divide'),
-]
+    path('', include('webapp.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
