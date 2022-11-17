fc7e241e5e3b04a12f426c4b8ab9acdb7316e8ac
Mosunov Anton <antonmossunov@gmail.com>
2022-11-17 22:13:50 +0600

    Реализация функционала для вычиcления произведения двух значений
    Создано представление, настроена маршрутизация

--- calculator/calculator/urls.py
+++ calculator/calculator/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import add, subtract
+from core.views import add, subtract, multiply
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('add/', add, name='add'),
     path('subtract/', subtract, name='subtract'),
+    path('multiply/', multiply, name='multiply'),
 ]
--- calculator/calculator/views.py
+++ calculator/calculator/views.py
@@ -24,9 +24,24 @@ def subtract(request, *args, **kwargs):
     if request.body:
         numbers = json.loads(request.body)
         try:
-            number_a = int(numbers['A'])
-            number_b = int(numbers['B'])
+            number_a = float(numbers['A'])
+            number_b = float(numbers['B'])
             result = number_a - number_b
+        except TypeError:
+            answer['error'] = 'There is a non-numeric value among the entered values'
+            return JsonResponse(answer, status=400)
+    answer['result'] = result
+    return JsonResponse(answer)
+
+
+def multiply(request, *args, **kwargs):
+    answer = {}
+    if request.body:
+        numbers = json.loads(request.body)
+        try:
+            number_a = float(numbers['A'])
+            number_b = float(numbers['B'])
+            result = number_a * number_b
         except TypeError:
             answer['error'] = 'There is a non-numeric value among the entered values'
             return JsonResponse(answer, status=400)
