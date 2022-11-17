3e7f67d216a42432b9efc951756d87976a1e8ebd
Mosunov Anton <antonmossunov@gmail.com>
2022-11-17 22:42:32 +0600

    Реализация функционала срабатывания кнопок.
    Каждой кнопке соотвутствует своя реализация в js
    После ввода пользователем чисел и нажатия на кнопку с выбранной операцией срабатывает ajax запрос на сервер, на котором в соответствии с указанным url срабатывает функционал того или иного представления. Результат вычислений выводится в специальном поле на стороне клиента в шаблоне

---
+++ calculator/static/js/calculate.js
@@ -0,0 +1,105 @@
+window.addEventListener('load', function() {
+
+    function getCookie(c_name) {
+        if(document.cookie.length > 0) {
+            c_start = document.cookie.indexOf(c_name + "=");
+            if(c_start != -1) {
+                c_start = c_start + c_name.length + 1;
+                c_end = document.cookie.indexOf(";", c_start);
+                if(c_end == -1) c_end = document.cookie.length;
+                return unescape(document.cookie.substring(c_start,c_end));
+            }
+        }
+        return "";
+    }
+
+
+    let buttonSubtract = $('#subtract');
+    buttonSubtract.on('click', function(evt) {
+        evt.preventDefault();
+        $('#result').val('');
+        $.ajax({
+            headers: {
+                "X-CSRFToken": getCookie("csrftoken")
+            },
+            url: 'subtract/',
+            type: 'POST',
+            data: JSON.stringify({
+                'A': $('#number-a').val(),
+                'B': $('#number-b').val(),
+            }),
+        }).done(
+            function(data) {
+                $('#result').append(data['result'])
+                console.log()
+            }
+        )
+    });
+
+
+    let buttonAdd = $('#calculate-add');
+    buttonAdd.on('click', function(evt) {
+        evt.preventDefault();
+        // $('#result').val('');
+        $.ajax({
+            headers: {
+                "X-CSRFToken": getCookie("csrftoken")
+            },
+            url: 'add/',
+            type: 'POST',
+            data: JSON.stringify({
+                'A': $('#number-a').val(),
+                'B': $('#number-b').val(),
+            }),
+        }).done(
+            function(data) {
+                $('#result').append(data['result'])
+                console.log()
+            }
+        )
+    });
+
+
+    let buttonMultiply = $('#multiply');
+    buttonMultiply.on('click', function(evt) {
+        evt.preventDefault();
+        $.ajax({
+            headers: {
+                "X-CSRFToken": getCookie("csrftoken")
+            },
+            url: 'multiply/',
+            type: 'POST',
+            data: JSON.stringify({
+                'A': $('#number-a').val(),
+                'B': $('#number-b').val(),
+            }),
+        }).done(
+            function(data) {
+                $('#result').append(data['result'])
+
+            }
+        )
+    });
+
+
+    let buttonDivide = $('#divide');
+    buttonDivide.on('click', function(evt) {
+        evt.preventDefault();
+        $.ajax({
+            headers: {
+                "X-CSRFToken": getCookie("csrftoken")
+            },
+            url: 'divide/',
+            type: 'POST',
+            data: JSON.stringify({
+                'A': $('#number-a').val(),
+                'B': $('#number-b').val(),
+            }),
+        }).done(
+            function(data) {
+                $('#result').append(data['result'])
+
+            }
+        )
+    });
+})
