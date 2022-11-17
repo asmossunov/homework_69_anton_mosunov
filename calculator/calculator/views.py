79ab5a2599c5981a2cd31cb57f7f09fa67db82f5
Mosunov Anton <antonmossunov@gmail.com>
2022-11-17 22:05:20 +0600

    Реализация функционала для сложения входящих значений.
    Создано представление, настроена маршрутизация

---
+++ calculator/calculator/views.py
@@ -0,0 +1,19 @@
+import json
+from django.http.response import JsonResponse
+
+
+def add(request, *args, **kwargs):
+    print(f'Распечатка входящего запроса! {request.body}')
+    print(request.POST)
+    answer = {}
+    if request.body:
+        numbers = json.loads(request.body)
+        try:
+            number_a = float(numbers['A'])
+            number_b = float(numbers['B'])
+            result = number_a + number_b
+            answer['result'] = result
+        except TypeError:
+            answer['error'] = 'There is a non-numeric value among the entered values'
+            return JsonResponse(answer, status=400)
+    return JsonResponse(answer)
